609349dd71e51237118292c1055f86497aed68ab
Castejon <castejonfernandez@gmail.com>
2019-12-12 13:30:18 +0100

    Update main.py
    El código no era correcto, estaba importado de un repositorio erróneo.

--- main.py
+++ main.py
@@ -1,48 +1,16 @@
-# Crea np_baseball (3 columnas)
-np_baseball = np.array(baseball)
-# Imprime la suma de np_baseball y updated
-np_baseball = np.array(updated)
-print(np_baseball)
+from xml.dom.minidom  import parse
+import xml.dom.minidom 
 
-# Crea un array numpy: conversion
-conversion = np.array([0.0254,0.453592,1])
-
-# Imprime el producto de np_baseball y conversion
-print (np_baseball*conversion)
-
-10.-
-def e_f(string)
-	string=string.split(' ')
-	return string
-	
-a = 'Hola esto es una cadena'
-
-e_f(a)
-
-11.-
-	
-
-13.-
-	B 
-	import pandas as pd
-	li=[100,200,300]
-	lg=[10,20,30]
-	def get_columns (lista1,lista2):
-		dataframe = pd.DataFrame({
-			'Ingresos': li,
-			'Gastos  ': lg
-		})
-		return dataframe
-	get_columns(lista1=li, lista2=lg)
-
-	C
-	import pandas as pd
-	li=[100,200,300]
-	lg=[10,20,30]
-	def get_columns (lista1,lista2):
-		dataframe['ingresos']=lista1
-		dataframe['gastos  ']=lista2
-		return dataframe
-
-	get_columns(lista1=li, lista2=lg)
-
+ArbolDom = xml.dom.minidom.parse("datos.xml")
+libros = ArbolDom.getElementsByTagName("Libro")
+for libro in libros:
+    isbn = libro.getAttribute("isbn")
+    print("isbn:", isbn)
+    titulo = libro.getElementsByTagName("titulo")[0]
+    print("titulo:", titulo.firstChild.data)
+    fecha = libro.getElementsByTagName("fecha")[0]
+    print("fecha:", fecha.firstChild.data)
+    autores = libro.getElementsByTagName("autor")
+    for autor in autores:
+        print("autor:", autor.firstChild.data)
+    print("-----")
